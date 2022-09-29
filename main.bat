768136f6af4eb2de99d8ed64c5946c0e1f89c002
洪靖翔 Sean <62828904+SeanHong9@users.noreply.github.com>
2022-09-30 01:35:43 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 @echo off
-set /a ori=%2,minus=2
-set /a result=%ori%-%minus%
+set /a origin=%2,minus=2
+set /a result=%origin%-%minus%
 
 for /l %%x in (%result% ,-1, 1)do (
    start alarm %1 %%x Taipei >nul
